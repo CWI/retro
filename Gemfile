a8fea4cc37a46ae94eeaec57fa3a11da16ac2ff3
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2013-06-21 19:16:45 -0300

    fixed #13 - including jquery_ujs back again.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
 gem 'mysql2'
+gem 'jquery-rails', '~> 2.1'
 
 group :assets do
   gem 'uglifier'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,12 +7,10 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require jquery_ujs
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-
-//= require_tree .
-//= require_self
 jQuery(function($) {
 
     // Retrospectives Alert Error
